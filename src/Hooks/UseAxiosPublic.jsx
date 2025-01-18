6c994f390c510df4723e9104f3edbcd5a730b6da
Rifat <rifatsakib353@hmail.com>
2025-01-18 19:25:13 +0600

    authprovider,login page working

---
+++ src/Hooks/UseAxiosPublic.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import axios from "axios";
+
+const axiosPublic = axios.create({
+    baseURL: 'https://bistro-boss-server-gamma-ashy.vercel.app'
+})
+
+const UseAxiosPublic = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default UseAxiosPublic;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import Main from '../Layout/Main';
 import Error from '../Pages/Shared/Error/Error';
 import Home from '../Pages/Home/Home/Home';
+import Login from '../Pages/Login/Login';
 
 export const router = createBrowserRouter([
   {
@@ -12,6 +13,10 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home/>,
+      },
+      {
+        path: 'login',
+        element: <Login></Login>
       },
     ],
   },
